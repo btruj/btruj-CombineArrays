ba24df871c056e8736dbac4927f3432992049fdb
Bryan Trujillo <trujillobryan99@yahoo.com>
2023-03-03 16:54:35 +0000

    complete

--- src/main/java/CombineArrays.java
+++ src/main/java/CombineArrays.java
@@ -9,6 +9,19 @@ public class CombineArrays {
      */
     public int[] combine(int[] arr1, int[] arr2){
 
-        return null;
+        int[] result = new int[arr1.length + arr2.length];
+        int index = 0;
+        for (int i = 0; i < arr1.length; i++) {
+            result[index] = arr1[i];
+            index++;
+        }
+    
+        for (int i = 0; i < arr2.length; i++) {
+            result[index] = arr2[i];
+            index++;
+        }
+    
+        return result;
     }
-}
+    
+    }
